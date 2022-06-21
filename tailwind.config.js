63a9835cc595d4ab173ae858567ee41a2de5580b
Koncept <68844962+Khynex@users.noreply.github.com>
2022-06-21 23:51:28 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            boxShadow: {
+                'bx': '0 2px 8px 0px rgba(99, 99, 99, 0.2)',
+            }
+        },
+    },
+    plugins: [],
+}
